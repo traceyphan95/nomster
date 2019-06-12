8617b8ccd3a3e86f26f71c73b1c833a51eede2b0
Tracey <traceyphan95@gmail.com>
2019-06-12 17:05:54 +0000

    Error for migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_174305) do
+ActiveRecord::Schema.define(version: 2019_06_12_163534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 2019_06_05_174305) do
     t.datetime "updated_at", null: false
     t.index ["place_id"], name: "index_comments_on_place_id"
     t.index ["user_id", "place_id"], name: "index_comments_on_user_id_and_place_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
   end
 
   create_table "places", force: :cascade do |t|
