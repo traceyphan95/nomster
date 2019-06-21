19730b035d1e8e06f6f9e7aafd3c79c730570cff
Tracey <traceyphan95@gmail.com>
2019-06-21 13:33:47 +0000

    Adding email basics

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,2 +1,8 @@
 class NotificationMailer < ApplicationMailer
+    default from: "no-reply@nomsterapp.com"
+
+  def comment_added
+        mail(to: "traceyphan95@gmail.com",
+        subject: "A comment has been added to your place")
+  end
 end
