e4fdd2410f09a1d2861f85492b0af3a2137bfec6
Tracey <traceyphan95@gmail.com>
2019-06-21 13:26:54 +0000

    Error fixed after adding photos option

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
-class User < ApplicationRecord; :Base
+class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
+
   has_many :places
   has_many :comments
-
+  has_many :photos
 end
--- db/migrate/20190620163124_add_photo_url_to_photos.rb
+++
@@ -1,5 +0,0 @@
-class AddPhotoUrlToPhotos < ActiveRecord::Migration[5.2]
-  def change
-    add_column :photos, :photo_url, :string
-  end
-end
